28aab81b946760c6d872a8159440b3550287e989
kumarprashant96 <kumarprashant96@hotmail.com>
2021-01-14 14:36:57 +0530

    script

---
+++ js/floating-whatsapp.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('#whatsAppDiv').floatingWhatsApp({
+        phone: '+919711017172',
+        popupMessage: 'Hello, how can we help you?',
+        message: "I want to purchase new lic",
+        headerTitle: 'Welcome!',
+        position: 'right',
+        showPopup: true
+    });
+});
